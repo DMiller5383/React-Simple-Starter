2ba238005fcf8bae965af2f14353bcde0930fa16
Daniel Miller <dmiller@cynosure.com>
2017-04-27 14:52:47 -0400

    started second section

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 //Create a new component.
 //This component shoudl produce some html
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyDleICMbIA_XQbBKLZi33fUeUMPbEhauEY';
-const App =() => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'},(videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+
 }
 
 
