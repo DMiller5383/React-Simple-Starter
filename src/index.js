6e8e6b1ec07d6cc6bcebb0012e1f86ea3702117f
Daniel Miller <dmiller@cynosure.com>
2017-04-30 16:26:08 -0400

    ajax with react

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDleICMbIA_XQbBKLZi33fUeUMPbEhauEY';
 
@@ -16,14 +17,15 @@ class App extends React.Component {
     this.state = {videos: []};
 
     YTSearch({key: API_KEY, term: 'surfboards'},(videos) => {
+      console.log(videos);
       this.setState({ videos });
     });
   }
   render() {
-
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
