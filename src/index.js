2fe2d589cb444fc1163620a8c232d5558405f609
Daniel Miller <daniel.miller5383@gmail.com>
2017-04-30 18:09:42 +0000

    wrapping api calls

--- src/index.js
+++ src/index.js
@@ -14,19 +14,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
-
-    YTSearch({key: API_KEY, term: 'surfboards'},(videos) => {
-      console.log(videos);
-      this.setState({ videos });
-    });
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+      this.videoSearch('surfboards');
   }
+
+
+    videoSearch(term) {
+      YTSearch({key: API_KEY, term: term},(videos) => {
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
+      });
+    }
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
