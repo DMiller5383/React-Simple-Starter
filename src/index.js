8a712e865c7d7682deef592b15cfedd7b0dc531b
Daniel Miller <dmiller@cynosure.com>
2017-05-01 08:25:00 -0400

    final version no redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //Create a new component.
-//This component shoudl produce some html
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -31,9 +31,10 @@ class App extends React.Component {
       });
     }
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar onSearchTermChange={term=>this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
